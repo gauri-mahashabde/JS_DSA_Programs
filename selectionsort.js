f2954e0ff65057251938aae90a0764b9e642b02d
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-24 21:41:14 +0530

    comments added

--- selectionsort.js
+++ selectionsort.js
@@ -6,20 +6,42 @@ document.getElementById("unsorted_array2").innerHTML = score;
 document.getElementById("selection_sort").innerHTML = selectionSort(score);
 
 function selectionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        console.log("outerloop iteration begin " + i + " array " + arr);
-        let min = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[min]) {
-                min = j;
-
-            }
-        }
-        console.log("min " + min + " i " + i);
-        swap(arr, min, i);
-        console.log("outerloop iteration end" + i + " array " + arr);
+  for (let i = 0; i < arr.length; i++) {
+    console.log("outerloop iteration begin " + i + " array " + arr);
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
     }
-    return arr;
+    console.log("min " + min + " i " + i);
+    swap(arr, min, i);
+    console.log("outerloop iteration end" + i + " array " + arr);
+  }
+  return arr;
 }
 
 //cleaner code with comments
+
+// Selection sort in JavaScript
+function swap(arr, xp, yp) {
+  var temp = arr[xp];
+  arr[xp] = arr[yp];
+  arr[yp] = temp;
+}
+function selectionSort(arr) {
+  var i, j, minIndex;
+
+  // loop to iterate over the entire array
+  for (i = 0; i < arr.length - 1; i++) {
+    // set minIndex equal to the first unsorted element
+    minIndex = i;
+
+    // iterate over unsorted sublist
+    for (j = i + 1; j < arr.length; j++)
+      if (arr[j] < arr[minIndex]) minIndex = j;
+
+    // swapping the minimum element with the element at minIndex
+    swap(arr, minIndex, i);
+  }
+}
