e1ebad77b292ca5f1258b4846efdce473460eb46
Gauri Mahashabde <gauri2interact@gmail.com>
2023-07-03 19:42:41 +0530

    insertion sort

--- insertionsort.js
+++ insertionsort.js
@@ -1,1 +1,13 @@
 //Insertion sort is a sorting algorithm in which the elements are transferred one at a time to the right position.
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let currentValue = arr[i]
+      let j
+      for (j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+        arr[j + 1] = arr[j]
+      }
+      arr[j + 1] = currentValue
+    }
+    return arr
+  }
