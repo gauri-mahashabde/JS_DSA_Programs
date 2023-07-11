b7f4b00795a45b825e4fc11b37225048c9eb4cb7
Gauri Mahashabde <gauri2interact@gmail.com>
2023-07-11 21:30:38 +0530

    slection sort syntax

--- insertionsort.js
+++ insertionsort.js
@@ -1,5 +1,30 @@
 //Insertion sort is a sorting algorithm in which the elements are transferred one at a time to the right position.
 
+//syntax code
+/*begin selectionSort(list)
+  for i = 0 to sizeof(list) - 1    
+      
+       minIndex = i;
+      
+       for j = i + 1 to sizeof(list)
+          
+           if list[j] < list[mid_index]
+               minIndex = j;
+           end if
+      
+          swap(list[minIndex], list[i])
+      
+       end for
+ 
+  end for
+end selectionSort*/
+
+
+
+
+
+
+
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
       let currentValue = arr[i]
@@ -11,3 +36,4 @@ function insertionSort(arr) {
     }
     return arr
   } 
+
